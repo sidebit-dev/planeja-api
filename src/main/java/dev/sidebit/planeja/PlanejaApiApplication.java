06e88fac07d1ac9e5d0418c5c1000de9edbc9c07
Miguel Arcanjo <miguel.arcanjo@outlook.com>
2026-09-19 10:34:15 -0300

    creates the project structure

---
+++ src/main/java/dev/sidebit/planeja/PlanejaApiApplication.java
@@ -0,0 +1,13 @@
+package dev.sidebit.planeja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanejaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanejaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sidebit/planeja/PlanejaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sidebit.planeja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanejaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
