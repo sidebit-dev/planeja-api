99db7dbaaca0bde830b1979e907bab10b624c310
Miguel Arcanjo <miguel.arcanjo@outlook.com>
2026-09-19 15:19:09 -0300

    Created and tested the card repository.

---
+++ src/main/java/dev/sidebit/planeja/domain/card/CardRepository.java
@@ -0,0 +1,9 @@
+package dev.sidebit.planeja.domain.card;
+
+import dev.sidebit.planeja.domain.card.model.CardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CardRepository extends JpaRepository<CardEntity, UUID> {
+}
---
+++ src/main/java/dev/sidebit/planeja/infra/Sandbox.java
@@ -0,0 +1,28 @@
+package dev.sidebit.planeja.infra;
+
+import dev.sidebit.planeja.domain.card.CardRepository;
+import dev.sidebit.planeja.domain.card.model.CardEntity;
+import dev.sidebit.planeja.domain.card.model.CardNetwork;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sandbox implements CommandLineRunner {
+//    Esta classe é para fazer testes
+    @Autowired
+    private CardRepository repository;
+
+    public void salvarCartao(){
+        CardEntity card = new CardEntity();
+        card.setName("ITAU Personalité");
+        card.setCardNetwork(CardNetwork.AMERICAN_EXPRESS);
+
+        repository.save(card);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        salvarCartao();
+    }
+}
