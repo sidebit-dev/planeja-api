82700a83b0ea7691cc41571058a16188664985c2
Miguel Arcanjo <miguel.arcanjo@outlook.com>
2026-09-19 22:35:36 -0300

    Implements DTO mappers using MapStruct.

--- src/main/java/dev/sidebit/planeja/domain/card/mapper/CardMapper.java
+++ src/main/java/dev/sidebit/planeja/domain/card/mapper/CardMapper.java
@@ -3,7 +3,9 @@ package dev.sidebit.planeja.domain.card.mapper;
 import dev.sidebit.planeja.domain.card.dto.CardDetail;
 import dev.sidebit.planeja.domain.card.dto.CardForm;
 import dev.sidebit.planeja.domain.card.model.CardEntity;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface CardMapper {
     CardEntity toEntity(CardForm form);
 
